c779a5a38cf19879ef88fea3299205c7beceabad
Rodrigo Casagrande de Oliveira <rodrigo.casagrande.oliveira@gmail.com>
2014-10-06 15:05:12 -0300

    Create the upmundo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Upmundo::Application.config.secret_key_base = '07e2e71fce7a2968dcb437dd7ab58deeaabfd3ac62db746adeb9c80e40079e1083955877a4b0d5f4a89a39e27d75aaea174be52fa2b71beb748db217f3932fad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Upmundo::Application.config.session_store :cookie_store, key: '_upmundo_session'
